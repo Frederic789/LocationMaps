a2ae030bb1908ac5389a627536958f795d64fb91
Freddy <fredericdouamba2014@gmail.com>
2023-11-25 20:05:37 -0800

    Initial Commit

---
+++ LocationMaps/LocationMapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationMapsApp.swift
+//  LocationMaps
+//
+//  Created by Student Account on 11/25/23.
+//
+
+import SwiftUI
+
+@main
+struct LocationMapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
